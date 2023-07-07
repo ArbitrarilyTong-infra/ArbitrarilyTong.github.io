dde1934c96e77e475d449ef8180e45092bc4baa3
easterNday <44698238+easterNday@users.noreply.github.com>
2023-07-07 08:23:26 +0000

    Update Update to output generated at 6e65508

--- update_sync.py
+++ update_sync.py
@@ -69,7 +69,7 @@ def generate_kernel_release_dict(file_info, release_name, device_name: str, rele
 # TODO
 def generate_system_release_dict(file_info, release_name):
     return {
-        "datetime": str(datetime.strptime(file_info["updated_at"], '%Y-%m-%dT%H:%M:%SZ')),
+        "datetime": int(datetime.strptime(file_info["updated_at"], '%Y-%m-%dT%H:%M:%SZ').timestamp()),
         "filename": file_info["name"],
         "id": file_info["id"],
         "tag": "Tong",
